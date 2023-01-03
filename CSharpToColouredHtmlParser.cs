d3c0b07325c0caaa2cb953c7c14f4014748c0b36
Roland Braun <dev@rolandbraun.com>
2023-01-03 18:50:36 +0100

    Add project files.

---
+++ CSharpToColouredHtmlParser.cs
@@ -0,0 +1,84 @@
+﻿using CsharpToColouredHTML.Core;
+using StatiCSharp.Interfaces;
+using System.Text;
+
+namespace StatiCSharp.Parser;
+
+public class CSharpToColouredHtmlParser : IPipelineParser
+{
+    private bool _disableLineNumbers = false;
+    private string _openingMarker = "```cs";
+    private string _closingMarker = "```";
+    public string HeaderContent => string.Empty;
+
+    public CSharpToColouredHtmlParser DisableLineNumbers()
+    {
+        _disableLineNumbers = true;
+        return this;
+    }
+
+    public string Parse(string input)
+    {
+        input = ReplaceMarkerVariations(input, _openingMarker);
+
+        var settings = new HTMLEmitterSettings()
+            .DisableIframe()
+            .UseCustomCSS(CustomCss.WithLineNumbers);
+
+        if (_disableLineNumbers)
+        {
+            settings.DisableLineNumbers()
+                .UseCustomCSS(CustomCss.WithoutLineNumbers);
+        }
+
+        var markers = FindMarkers(input, _openingMarker, _closingMarker);
+        while (markers != (-1, -1))
+        {
+            string leadingText = input.Substring(0, markers.Leading);
+            
+            string codeText = input.Substring(
+                markers.Leading + _openingMarker.Length,
+                markers.Trailing - markers.Leading - _openingMarker.Length
+                ).Trim();
+            
+            string trailingText = input.Substring(markers.Trailing + _closingMarker.Length);
+
+            codeText = new CsharpColourer().ProcessSourceCode(codeText, new HTMLEmitter(settings));
+
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append(leadingText);
+            stringBuilder.Append(codeText);
+            stringBuilder.Append(trailingText);
+            input = stringBuilder.ToString();
+            markers = FindMarkers(input, _openingMarker, _closingMarker);
+        }
+
+        return input;
+    }
+
+    private (int Leading, int Trailing) FindMarkers(string input, string openingMarker, string closingMarker)
+    {
+        int indexOpening = 0;
+        int indexClosing = 0;
+
+        indexOpening = input.IndexOf(openingMarker);
+        indexClosing = input.IndexOf(closingMarker, indexOpening + 1);
+
+        if (indexOpening != -1 && indexClosing != -1)
+        {
+            return (indexOpening, indexClosing);
+        }
+        return (-1, -1);
+    }
+
+    private string ReplaceMarkerVariations(string input, string marker)
+    {
+        input = input.Replace("```csharp", marker);
+        input = input.Replace("```Csharp", marker);
+        input = input.Replace("```CSharp", marker);
+        input = input.Replace("```C#", marker);
+        input = input.Replace("```c#", marker);
+
+        return input;
+    }
+}
---
+++ CustomCss.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition.Convention;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatiCSharp.Parser;
+
+internal static class CustomCss
+{
+    public static string WithLineNumbers
+    {
+        get => """
+                <style>
+                .background {
+                font-family: monaco, Consolas, LucidaConsole, monospace;
+                background-color: #1e1e1e;
+                overflow: auto;
+                padding: 20px;
+                font-size: 0.8em;
+                }
+                pre .numeric {
+                  color: #b5cea8;
+                }
+                pre .method {
+                  color: #dcdcaa;
+                }
+                pre .class {
+                  color: #4ec9b0;
+                }
+                pre .keyword {
+                  color: #569cd6;
+                }
+                pre .string {
+                  color: #ce9178;
+                }
+                pre .interface {
+                  color: #b8d7a3;
+                }
+                pre .enumName {
+                  color: #b8d7a3;
+                }
+                pre .numericLiteral {
+                  color: #b8d7a3;
+                }
+                pre .recordStruct {
+                  color: #b8d7a3;
+                }
+                pre .typeParam {
+                  color: #b8d7a3;
+                }
+                pre .extension {
+                  color: #b8d7a3;
+                }
+                pre .control {
+                  color: #c586c0;
+                }
+                pre .internalError {
+                  color: #ff0d0d;
+                }
+                pre .comment {
+                  color: #6a9955;
+                }
+                pre .preprocessor {
+                  color: #808080;
+                }
+                pre .preprocessorText {
+                  color: #a4a4a4;
+                }
+                pre .struct {
+                  color: #86c691;
+                }
+                pre .namespace {
+                  color: #dfdfdf;
+                }
+                pre .enumMember {
+                  color: #dfdfdf;
+                }
+                pre .identifier {
+                  color: #dfdfdf;
+                }
+                pre .punctuation {
+                  color: #dfdfdf;
+                }
+                pre .operator {
+                  color: #dfdfdf;
+                }
+                pre .propertyName {
+                  color: #dfdfdf;
+                }
+                pre .fieldName {
+                  color: #dfdfdf;
+                }
+                pre .labelName {
+                  color: #dfdfdf;
+                }
+                pre .operator_overloaded {
+                  color: #dfdfdf;
+                }
+                pre .constant {
+                  color: #dfdfdf;
+                }
+                pre .localName {
+                  color: #9cdcfe;
+                }
+                pre .parameter {
+                  color: #9cdcfe;
+                }
+                pre .delegate {
+                  color: #4ec9b0;
+                }
+                pre .eventName {
+                  color: #dfdfdf;
+                }
+                pre .excludedCode {
+                  color: #808080;
+                }
+                pre table {
+                  white-space: pre;
+                }
+                pre .line_no::before {
+                  content: attr(line_no);
+                  color: white;
+                }
+                pre .code_column {
+                  padding-left: 5px;
+                }
+                pre {
+                  color: #dbebeb;
+                }
+                </style>
+                """;
+    }
+
+    public static string WithoutLineNumbers
+    {
+        get => """
+                <style>
+                .background {
+                font-family: monaco, Consolas, LucidaConsole, monospace;
+                background-color: #1e1e1e;
+                overflow: auto;
+                padding: 20px;
+                font-size: 0.8em;
+                }
+                pre .numeric {
+                  color: #b5cea8;
+                }
+                pre .method {
+                  color: #dcdcaa;
+                }
+                pre .class {
+                  color: #4ec9b0;
+                }
+                pre .keyword {
+                  color: #569cd6;
+                }
+                pre .string {
+                  color: #ce9178;
+                }
+                pre .interface {
+                  color: #b8d7a3;
+                }
+                pre .enumName {
+                  color: #b8d7a3;
+                }
+                pre .numericLiteral {
+                  color: #b8d7a3;
+                }
+                pre .recordStruct {
+                  color: #b8d7a3;
+                }
+                pre .typeParam {
+                  color: #b8d7a3;
+                }
+                pre .extension {
+                  color: #b8d7a3;
+                }
+                pre .control {
+                  color: #c586c0;
+                }
+                pre .internalError {
+                  color: #ff0d0d;
+                }
+                pre .comment {
+                  color: #6a9955;
+                }
+                pre .preprocessor {
+                  color: #808080;
+                }
+                pre .preprocessorText {
+                  color: #a4a4a4;
+                }
+                pre .struct {
+                  color: #86c691;
+                }
+                pre .namespace {
+                  color: #dfdfdf;
+                }
+                pre .enumMember {
+                  color: #dfdfdf;
+                }
+                pre .identifier {
+                  color: #dfdfdf;
+                }
+                .punctuation {
+                  color: #dfdfdf;
+                }
+                pre .operator {
+                  color: #dfdfdf;
+                }
+                pre .propertyName {
+                  color: #dfdfdf;
+                }
+                pre .fieldName {
+                  color: #dfdfdf;
+                }
+                pre .labelName {
+                  color: #dfdfdf;
+                }
+                pre .operator_overloaded {
+                  color: #dfdfdf;
+                }
+                pre .constant {
+                  color: #dfdfdf;
+                }
+                pre .localName {
+                  color: #9cdcfe;
+                }
+                pre .parameter {
+                  color: #9cdcfe;
+                }
+                pre .delegate {
+                  color: #4ec9b0;
+                }
+                pre .eventName {
+                  color: #dfdfdf;
+                }
+                pre .excludedCode {
+                  color: #808080;
+                }
+                pre {
+                  color: #dbebeb;
+                }
+                </style>
+                """;
+    }
+}
