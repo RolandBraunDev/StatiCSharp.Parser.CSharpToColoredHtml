2cabf979dc6a5144dea88a350355e62ede7bf0b8
Roland Braun <dev@rolandbraun.com>
2023-04-07 09:42:15 +0200

    Update: Changed namespace
    Changed namespace of the class to StatiCSharp.Parser.CSharpToColoredHtmlParser.

--- src/CustomCss.cs
+++ src/CustomCss.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StatiCSharp.Parser;
+namespace StatiCSharp.Parser.CSharpToColoredHtmlParser;
 
 internal static class CustomCss
 {
